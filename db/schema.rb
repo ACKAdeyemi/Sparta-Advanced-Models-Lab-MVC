6dfd81736af502801fdbbe43d31e286a9b9759e4
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-06-20 11:48:52 +0100

    Relationship between Country and Author established

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_103231) do
+ActiveRecord::Schema.define(version: 2018_06_20_104137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_06_20_103231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "dob"
+    t.integer "country_id"
   end
 
   create_table "books", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@
 end
 
 3.times do
-  Author.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, dob: Faker::Date.between_except(100.year.ago, 15.year.ago, Date.today)) #=> DATE FORMAT: "Wed, 24 Sep 2014"
+  rand_country = rand(1..Country.all.length)
+  Author.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, dob: Faker::Date.between_except(100.year.ago, 15.year.ago, Date.today), country_id: rand_country) #=> DATE FORMAT: "Wed, 24 Sep 2014"
 end
 
 3.times do
