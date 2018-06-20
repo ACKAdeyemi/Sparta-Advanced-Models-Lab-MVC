2b92bf34f23d1f93f7b070c288df9df729089c96
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2018-06-20 11:04:50 +0100

    Set up models

---
+++ db/migrate/20180620100223_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :d_of_b
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180620100343_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :publihed_year
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
