18e2dfe31fab5fa8e4eabce714b71415fbc7a610
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-06-21 09:04:22 +0100

    Task complete - my add paragraph link is broken, troubleshooting now

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
   belongs_to :country
   has_many :books
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :countries
+  resources :authors
+  resources :books
+  resources :chapters
+  resources :paragraphs
 end
